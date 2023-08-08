5bb9b86e5c9dc7f08e6d1a684aa12e29d2e641ac
Lalit Mahale <105535508+LalitMahale@users.noreply.github.com>
2023-08-08 08:13:03 +0530

    Update streamlit_app.py
    to see whole data sidebar updated
    Contact button also added

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,14 +10,13 @@ key = "d04zgljxeva_WbFhwMJowwp3B7vNgdQViDoDqxfMRFVc"
 
 deta = Deta(key)
 
-def push_data(db,name,heigth):
+def push_data(db,name,heigth,sr):
     # st.write(db.fetch().count)
-    return db.put({"key":str(db.fetch().count+1),"झाडाचे_नाव":name,"उंची":str(heigth)})
+    return db.put({"key":sr,"झाडाचे_नाव":name,"उंची":str(heigth)})
 
 def update(db,key,name,heigth):
     # st.write(db.fetch().count)
     return db1.update(updates={"झाडाचे_नाव":name,"उंची":str(height)},key=str(key))
-
 
 st.markdown("""<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">""", unsafe_allow_html=True)
 
@@ -39,13 +38,14 @@ st.markdown("<h1 style='text-align: center; font-size: 28px;'> <i class='fa fa-t
 # st.sidebar.header("प्लॉट निवडा")
 plot = st.selectbox("प्लॉट निवडा",["","सरिता पॉलिमर्स","नारायण", "लक्ष्मी ऍग्रो","राकेश ब्रिक्स","सुमरशिंग" ])
 
+
 if plot == "सरिता पॉलिमर्स":
     st.title("सरिता पॉलिमर्स")
     db1 = deta.Base("sarita")
     name = st.selectbox("झाडाचे नाव निवडा",["","कडू लिंब","कविट","वड","आंब्या","पिंपळ","निलगि","उपलब्ध नाही"])
     if name == "उपलब्ध नाही":
         name = st.text_input("झाडाचे नाव टाका")
-    if name == "":
+    if name == "" or name == " ":
         st.error("कृपया वैध नाव प्रविष्ट करा")
     height = st.number_input("झाडांची उंची",step=1,min_value=1)
 
@@ -55,7 +55,9 @@ if plot == "सरिता पॉलिमर्स":
     button_update = st.button("बदल करा")
 
     if button_save :
+        sr = str(db1.fetch().count+1)
         push_data(db=db1, name=name,heigth=height)
+        st.write("key :",sr)
         st.write(f"झाडाचे नाव :{name }")
         st.write(f"झाडांची उंची : {height }")
         st.success("यशस्वीरित्या जतन केले ")
@@ -87,7 +89,12 @@ elif plot == "नारायण":
     button = st.button("जतन करा")
     if button:
         # sr = db1.fetch().count + 1
-        push_data(db=db2, name=name,heigth=height)
+        sr = str(db2.fetch().count+1)
+        push_data(db=db2, name=name,heigth=height,sr=sr)
+
+        st.write("key :",sr)
+
+
         st.write(f"झाडाचे नाव :{name }")
         st.write(f"झाडांची उंची : {height }")
         st.success("यशस्वीरित्या जतन केले ")
@@ -100,12 +107,15 @@ elif plot == "लक्ष्मी ऍग्रो":
     name = st.selectbox("झाडाचे नाव निवडा",["","कडू लिंब","कविट","वड","आंब्या","पिंपळ","निलगि","उपलब्ध नाही"])
     if name == "उपलब्ध नाही":
         name = st.text_input("झाडाचे नाव टाका")
-    if name == "":
+    if name == "" or name == " ":
         st.error("कृपया वैध नाव प्रविष्ट करा")
     height = st.number_input("झाडांची उंची",step=1,min_value=1)
     button = st.button("जतन करा")
     if button:
-        push_data(db=db3, name=name,heigth=height)
+        sr = str(db3.fetch().count+1)
+        push_data(db=db3, name=name,heigth=height,sr=sr)
+        st.write("key :",sr)
+
         st.write(f"झाडाचे नाव :{name }")
         st.write(f"झाडांची उंची : {height }")
         st.success("यशस्वीरित्या जतन केले ")
@@ -118,13 +128,15 @@ elif plot == "राकेश ब्रिक्स":
     name = st.selectbox("झाडाचे नाव निवडा",["","कडू लिंब","कविट","वड","आंब्या","पिंपळ","निलगि","उपलब्ध नाही"])
     if name == "उपलब्ध नाही":
         name = st.text_input("झाडाचे नाव टाका")
-    if name == "":
+    if name == "" or name == " ":
         st.error("कृपया वैध नाव प्रविष्ट करा")
     height = st.number_input("झाडांची उंची",step=1,min_value=1)
     button = st.button("जतन करा")
     if button:
         # sr = db1.fetch().count + 1
+        sr = str(db4.fetch().count+1)
         push_data(db=db4, name=name,heigth=height)
+        st.write("key :",sr)
         st.write(f"झाडाचे नाव :{name }")
         st.write(f"झाडांची उंची : {height }")
         st.success("यशस्वीरित्या जतन केले ")
@@ -138,17 +150,70 @@ elif plot == "सुमरशिंग":
     name = st.selectbox("झाडाचे नाव निवडा",["","कडू लिंब","कविट","वड","आंब्या","पिंपळ","निलगि","उपलब्ध नाही"])
     if name == "उपलब्ध नाही":
         name = st.text_input("झाडाचे नाव टाका")
-    if name == "":
+    if name == "" or name == " ":
         st.error("कृपया वैध नाव प्रविष्ट करा")
     height = st.number_input("झाडांची उंची",step=1,min_value=1)
     button = st.button("जतन करा")
     if button:
-        # sr = db1.fetch().count + 1
+        sr = str(db5.fetch().count+1)
         push_data(db=db5, name=name,heigth=height)
+        st.write("key :",sr)
         st.write(f"झाडाचे नाव :{name }")
         st.write(f"झाडांची उंची : {height }")
         st.success("यशस्वीरित्या जतन केले ")
         # st.write(db1.fetch().items)
         st.write(pd.DataFrame(db5.fetch().items).tail(5))
 
+
+
+bar = st.sidebar.selectbox("संपूर्ण डेटा पाहण्यासाठी प्लॉट निवडा",["","सरिता पॉलिमर्स","नारायण", "लक्ष्मी ऍग्रो","राकेश ब्रिक्स","सुमरशिंग" ])
+
+if bar == "सरिता पॉलिमर्स":
+    st.sidebar.write("निवडले :- सरिता पॉलिमर्स")
+    db1 = deta.Base("sarita")
+    b = st.sidebar.button("पहा")
+    if b:
+        st.sidebar.write(pd.DataFrame(db1.fetch().items))
+
+elif bar == "नारायण":
+    st.sidebar.write("निवडले :- नारायण")
+    db1 = deta.Base("narayan")
+    b = st.sidebar.button("पहा")
+    if b:
+        st.sidebar.write(pd.DataFrame(db1.fetch().items))
+
     
+elif bar == "लक्ष्मी ऍग्रो":
+    st.sidebar.write("निवडले :- लक्ष्मी ऍग्रो")
+    db1 = deta.Base("laxmi")
+    b = st.sidebar.button("पहा")
+    if b:
+        st.sidebar.write(pd.DataFrame(db1.fetch().items))
+
+elif bar == "राकेश ब्रिक्स":
+    st.sidebar.write("निवडले :- राकेश ब्रिक्स")
+    db1 = deta.Base("rakesh")
+    b = st.sidebar.button("पहा")
+    if b:
+        st.sidebar.write(pd.DataFrame(db1.fetch().items))
+
+elif bar == "सुमरशिंग":
+    st.sidebar.write("निवडले :- सुमरशिंग")
+    db1 = deta.Base("sumershing")
+    b = st.sidebar.button("पहा")
+    if b:
+        st.sidebar.write(pd.DataFrame(db1.fetch().items))
+
+
+
+
+st.markdown("<div style='height: 15vh'></div>", unsafe_allow_html=True)
+
+# Create a container to hold the "Contact Us" button
+contact_container = st.container()
+with contact_container:
+        # Right-align the button at the bottom
+        st.markdown("<div style='position: fixed; bottom: 10px; right: 10px; text-align: right;'>"
+                    "<a href='tel:+918087830153' target='_blank'>"
+                    "<button style='padding: 10px 20px;'>संपर्क साधा</button>"
+                    "</a></div>", unsafe_allow_html=True)
