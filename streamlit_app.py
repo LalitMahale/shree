78af743514e1bcdba0ecbea705fdacdde0e3d0a8
Lalit Mahale <105535508+LalitMahale@users.noreply.github.com>
2023-08-12 21:49:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,25 @@ from deta import Deta
 # from dotenv import load_dotenv
 # load_dotenv(".env")
 # key = os.getenv("key")
+
+custom_css = """
+<style>
+body {
+    margin: 0;
+    padding: 0;
+}
+</style>
+"""
+st.markdown(custom_css, unsafe_allow_html=True)
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 hide_st_style = """
             <style>
             #MainMenu {visibility: hidden;}
